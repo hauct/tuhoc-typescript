d8c0cb6e9961b7cf292e8e2c7086fffc39ebfdaf
cungtronghau <cungtronghau@gmail.com>
2025-10-27 01:30:29 +0700

    edit

---
+++ app.js
@@ -0,0 +1,19 @@
+// const nameTS1 = "Eric";
+// console.log("length = ", nameTS1.length);
+// console.log("upPerCase = ", nameTS1.toUpperCase());
+// let count: number = 2;
+// console.log(`Số đã nhập là ${count}`);
+// let mang = ["Hello", "It", "is", "an", "array"];
+// mang.push("25");
+// console.log(`Mảng là ${mang}`);
+// let numberTest: number = 9.6;
+// console.log(`Số là ${numberTest}`);
+// let textTest = `
+//     Đây là đoạn test "hello"
+// `;
+// console.log(`Text là ${textTest}`);
+var person = {
+    name: "Hau",
+    age: "24",
+};
+console.log("T\u00EAn l\u00E0 ".concat(person));
